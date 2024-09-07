5e4b29f6fa158a8978222668bf76b662bc026a08
João Mário <marioartec39@gmail.com>
2024-09-07 18:58:48 -0300

    Update Token Routine

--- src/main/kotlin/com/sanisamoj/config/Config.kt
+++ src/main/kotlin/com/sanisamoj/config/Config.kt
@@ -3,12 +3,21 @@ package com.sanisamoj.config
 import com.sanisamoj.data.models.interfaces.BotRepository
 import com.sanisamoj.database.mongodb.MongoDatabase
 import com.sanisamoj.database.redis.Redis
+import com.sanisamoj.utils.schedule.ScheduleRoutine
+import com.sanisamoj.utils.schedule.models.JobIdentification
+import com.sanisamoj.utils.schedule.models.RoutineGroups
+import com.sanisamoj.utils.schedule.models.StartRoutineData
+import com.sanisamoj.utils.schedule.routines.UpdateBotApiToken
+import org.quartz.JobKey
 
 object Config {
+    private val jobsIdentificationList: MutableList<JobIdentification> = mutableListOf()
+    private const val EVERY_TWO_YEARS_CRON: String = "0 0 0 1 1 ? */2"
 
     suspend fun initialize() {
         databaseInitialize()
         botApiInitialize()
+        updateBotApiToken()
     }
 
     private suspend fun databaseInitialize() {
@@ -20,4 +29,18 @@ object Config {
         val botRepository: BotRepository = GlobalContext.botRepository
         botRepository.updateToken()
     }
+
+    private fun updateBotApiToken() {
+        val routineName = "UpdateBotApiToken-EveryTwoYears_At00"
+        val startRoutineData = StartRoutineData(
+            name = routineName,
+            group = RoutineGroups.TokenUpdate,
+            cronExpression = EVERY_TWO_YEARS_CRON
+        )
+
+        val jobKey: JobKey = ScheduleRoutine().startRoutine<UpdateBotApiToken>(startRoutineData)
+        val jobIdentification = JobIdentification(jobKey, routineName)
+        jobsIdentificationList.add(jobIdentification)
+    }
+
 }
