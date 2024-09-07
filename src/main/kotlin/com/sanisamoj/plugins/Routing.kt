7cfaab87dfd6da27815d4ada6e93196978392c23
João Mário <marioartec39@gmail.com>
2024-09-07 18:41:12 -0300

    Get/ Removed

--- src/main/kotlin/com/sanisamoj/plugins/Routing.kt
+++ src/main/kotlin/com/sanisamoj/plugins/Routing.kt
@@ -4,7 +4,6 @@ import com.sanisamoj.routing.applicationServiceRouting
 import com.sanisamoj.routing.logEventRouting
 import com.sanisamoj.routing.operatorRouting
 import io.ktor.server.application.*
-import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
