c8f8032930e45f3f18e5f8afbb015eca03bf2df7
João Mário <marioartec39@gmail.com>
2024-09-07 16:06:08 -0300

    serverTestConfirmation

--- src/main/kotlin/com/sanisamoj/plugins/Routing.kt
+++ src/main/kotlin/com/sanisamoj/plugins/Routing.kt
@@ -4,6 +4,7 @@ import com.sanisamoj.routing.applicationServiceRouting
 import com.sanisamoj.routing.logEventRouting
 import com.sanisamoj.routing.operatorRouting
 import io.ktor.server.application.*
+import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
@@ -11,5 +12,9 @@ fun Application.configureRouting() {
         operatorRouting()
         applicationServiceRouting()
         logEventRouting()
+
+        get("/test") {
+            return@get call.respond("server" to "ok")
+        }
     }
 }
