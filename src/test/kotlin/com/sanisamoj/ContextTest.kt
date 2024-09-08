dfbb9420340906a5c32e1cf392ac73a93cb4cec1
João Mário <marioartec39@gmail.com>
2024-09-07 23:09:57 -0300

    LoginTest

--- src/test/kotlin/com/sanisamoj/ContextTest.kt
+++ src/test/kotlin/com/sanisamoj/ContextTest.kt
@@ -4,21 +4,29 @@ import com.sanisamoj.config.GlobalContext
 import com.sanisamoj.data.models.dataclass.Operator
 import com.sanisamoj.data.models.enums.OperatorStatus
 import com.sanisamoj.data.models.interfaces.DatabaseRepository
+import com.sanisamoj.database.mongodb.Fields
+import com.sanisamoj.database.mongodb.OperationField
+import org.mindrot.jbcrypt.BCrypt
 
 object ContextTest {
     private val databaseRepository: DatabaseRepository by lazy { GlobalContext.databaseRepository }
-    private val operator  = Operator(
+    val operator  = Operator(
         name = "OperatorTestName",
         email = "operatorEmail@domain.test",
         phone = "111111111111",
         password = "passwordTest"
     )
 
-    lateinit var operatorTest: Operator
+    lateinit var operatorTestInDb: Operator
 
     suspend fun createOperatorTest(operatorStatus: OperatorStatus = OperatorStatus.Disabled): Operator {
-        val operatorInDb: Operator = databaseRepository.createOperator(operator)
-        operatorTest = operatorInDb
+        val hashedPassword = BCrypt.hashpw(operator.password, BCrypt.gensalt())
+        val operatorInDb: Operator = databaseRepository.createOperator(operator.copy(password = hashedPassword))
+        databaseRepository.updateOperator(
+            operatorId = operatorInDb.id.toString(),
+            update = OperationField(Fields.Status, operatorStatus.name)
+        )
+        operatorTestInDb = operatorInDb
         return operatorInDb
     }
 
--- src/test/kotlin/com/sanisamoj/services/OperatorAuthenticationServiceTest.kt
+++ src/test/kotlin/com/sanisamoj/services/OperatorAuthenticationServiceTest.kt
@@ -1,15 +1,40 @@
 package com.sanisamoj.services
 
-import com.sanisamoj.config.GlobalContext
-import com.sanisamoj.data.models.interfaces.DatabaseRepository
-import com.sanisamoj.data.models.interfaces.SessionRepository
+import com.sanisamoj.ContextTest
+import com.sanisamoj.config.GlobalContext.EMPTY_VALUE
+import com.sanisamoj.data.models.dataclass.Operator
+import com.sanisamoj.data.models.dataclass.OperatorLoginRequest
+import com.sanisamoj.data.models.dataclass.OperatorLoginResponse
+import com.sanisamoj.data.models.enums.OperatorStatus
+import com.sanisamoj.database.redis.Redis
+import com.sanisamoj.services.operator.OperatorAuthenticationService
 import com.sanisamoj.utils.eraseAllDataToTests
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
 
 class OperatorAuthenticationServiceTest {
-    private val databaseRepository: DatabaseRepository = GlobalContext.databaseRepository
-    private val sessionRepository: SessionRepository = GlobalContext.sessionRepository
 
     init {
+        Redis.initialize()
         eraseAllDataToTests()
+    }
+
+    @Test
+    fun loginTest() = testApplication {
+        val operator: Operator = ContextTest.createOperatorTest(OperatorStatus.Activated)
+        val operatorAuthenticationService = OperatorAuthenticationService()
+        val operatorLoginRequest = OperatorLoginRequest(operator.email, "wrong")
+        assertFails { operatorAuthenticationService.login(operatorLoginRequest) }
+
+        val login: OperatorLoginResponse = operatorAuthenticationService.login(operatorLoginRequest.copy(password = ContextTest.operator.password))
+        assertEquals(operator.id.toString(), login.account.id)
+        assertEquals(operator.name, login.account.name)
+        assertEquals(operator.email, login.account.email)
+        assertEquals(operator.phone, login.account.phone)
+        assertEquals(operator.profileImage, EMPTY_VALUE)
+
+        ContextTest.deleteOperator()
     }
 }
