8ebfdbc0c33e0e3adf5cd69b977970452fedf8ad
João Mário <marioartec39@gmail.com>
2024-09-16 00:52:00 -0300

    DefaultBotRepository.kt updated

--- src/main/kotlin/com/sanisamoj/data/repository/DefaultBotRepository.kt
+++ src/main/kotlin/com/sanisamoj/data/repository/DefaultBotRepository.kt
@@ -4,6 +4,8 @@ import com.sanisamoj.api.BotApiService
 import com.sanisamoj.data.models.dataclass.*
 import com.sanisamoj.data.models.interfaces.BotRepository
 import com.sanisamoj.utils.analyzers.dotEnv
+import kotlinx.coroutines.delay
+import java.util.concurrent.TimeUnit
 
 class DefaultBotRepository(
     private val botApiService: BotApiService
@@ -14,11 +16,19 @@ class DefaultBotRepository(
     private val botId: String by lazy { dotEnv("BOT_ID") }
 
     override suspend fun updateToken() {
-        val loginRequest = LoginRequest(email, password)
-        token = botApiService.login(loginRequest).token
+        try {
+            val loginRequest = LoginRequest(email, password)
+            token = botApiService.login(loginRequest).token
+            println("Bot token updated!")
+        } catch (_: Throwable) {
+            delay(TimeUnit.SECONDS.toMillis(60))
+            updateToken()
+        }
     }
 
     override suspend fun sendMessage(messageToSend: MessageToSend) {
-        botApiService.sendMessage(botId, messageToSend, "Bearer $token")
+        try {
+            botApiService.sendMessage(botId, messageToSend, "Bearer $token")
+        } catch (_: Throwable) {}
     }
 }
